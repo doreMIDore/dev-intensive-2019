3ee3a567ae5ea1cc812813e2ec93a02092366219
doreMIDore <Igor12341999@yandex.ru>
2020-10-01 19:15:12 +0300

    try#2 same FullName bl

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -25,7 +25,7 @@ data class User(
        private var lastId : Int = - 1
     fun makeUser(fullName : String?) : User {
         lastId++
-        val (firstName,lastName) = Utils.parseFullname(fullName)
+        val (firstName,lastName) = Utils.parseFullName(fullName)
             return User(id = "$lastId", firstName = firstName ,lastName = lastName)
     }
 }

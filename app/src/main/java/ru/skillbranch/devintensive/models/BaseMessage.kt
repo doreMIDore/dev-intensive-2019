4c75f120cfc27576346ee0c06ba70e563c2014e6
doreMIDore <Igor12341999@yandex.ru>
2020-10-01 19:53:36 +0300

    try#3  module2 java.lang 0_0 makemassage

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -12,11 +12,11 @@ abstract class BaseMessage (val id: String,
 
     companion object AbstractFactory {
             var lastId = -1
-        fun makeMessage(from: User?, chat: Chat,date: Date = Date (), type: String = "text" , payload : Any?) : BaseMessage{
+        fun makeMessage(from: User?, chat: Chat, isIncoming: Boolean = false, date: Date = Date (), type: String = "text" , payload : Any?) : BaseMessage{
             lastId++
             return when (type){
-                "image"-> ImageMessage("$lastId", from, chat, date = date, image = payload as String)
-                    else ->TextMessage("$lastId", from, chat, date = date, text = payload as String)
+                "image"-> ImageMessage("$lastId", from, chat, isIncoming=isIncoming, date = date, image = payload as String)
+                    else ->TextMessage("$lastId", from, chat, isIncoming=isIncoming, date = date, text = payload as String)
                 }
         }
 

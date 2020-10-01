b4e79e8d4d41c299317e2b499d5fd6a681347e33
doreMIDore <Igor12341999@yandex.ru>
2020-10-01 18:39:49 +0300

    test #2

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,60 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.lang.IllegalStateException
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val SECOND = 1000L
+const val MINUTES= SECOND*60
+const val HOUR= MINUTES*60
+const val DAY= HOUR*24
+
+fun Date.format(pattern : String = "HH:mm:ss dd.MM.yy") : String {
+    val Date = SimpleDateFormat(pattern, Locale("ru"))
+    return Date.format(this)
+}
+
+fun Date.add(value : Int, units: TimeUnits = TimeUnits.SECOND) : Date{
+    var time = time
+       time+=when(units) {
+           TimeUnits.SECOND -> value* SECOND
+           TimeUnits.MINUTE -> value* MINUTES
+           TimeUnits.HOUR -> value* HOUR
+           TimeUnits.DAY-> value* DAY
+       }
+    this.time=time
+    return this
+}
+fun Date.humanizeDiff(date:Date = Date()) : String =
+    when (Date().time - this.time) {
+        in 0..1000 -> "только что"
+        in 1000..45000 -> "несколько секунд назад"
+        in 45000..75000 -> "минуту назад"
+        in 75000..45 * MINUTES -> "${(Date().time - this.time) / MINUTES} минут назад"
+        in 45 * MINUTES..75 * MINUTES -> "Час назад"
+        in 75 * MINUTES..22 * HOUR -> "${(Date().time - this.time) / HOUR} часов назад"
+        in 22 * HOUR..26 * HOUR -> "день назад"
+        in 26 * HOUR..360 * DAY -> "${(Date().time - this.time) / DAY} дней назад"
+        else -> "более года назад"
+    }
+
+
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY;
+
+   /* fun plural(value:Int) :String{
+        TODO("NEED MORE KNOWLEDGE")
+        val plural : String =
+        when (value) {
+            in 0..1  -> if (this == TimeUnits.SECOND)  "$value секунду" else ""
+            in 2..4 -> "$value секунды"
+            in 5..10 -> "$value секунд"
+            else -> ""
+        }
+        return plural
+    }*/
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,111 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullname(fullname : String?) : Pair<String?,String?> {
+        val parts : List<String>? = fullname?.split(" ")
+        var firstName = parts?.getOrNull(0)
+        var lastName = parts?.getOrNull(1)
+        if (firstName.isNullOrBlank()) {
+            firstName = null
+            lastName = null
+        }
+        return firstName to lastName
+    }
+
+    fun toInitials(firstName : String?, lastName: String?) :  String?{
+            var firstName = firstName
+            var lastName = lastName
+        if (firstName.isNullOrBlank())
+                firstName = null
+        if (lastName.isNullOrBlank())
+            lastName = null
+        firstName = firstName?.dropWhile{
+            it == ' '
+        }
+        lastName = lastName?.dropWhile{
+            it == ' '
+        }
+         firstName  = firstName?.first()?.toUpperCase()?.toString()?:""
+         lastName  = lastName?.first()?.toUpperCase()?.toString() ?:""
+
+        var initials : String? = firstName+lastName
+        if (initials.isNullOrBlank())
+        initials=null
+        return initials
+    }
+
+    fun translitiration (divider  : String = "") : String =
+        divider.replace(Regex("[йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]")) {
+            when (it.value) {
+                "a" -> "a"
+                "б" -> "b"
+                "в" -> "v"
+                "г" -> "g"
+                "д" -> "d"
+                "е" -> "e"
+                "ё" -> "e"
+                "ж" -> "zh"
+                "з" -> "z"
+                "и" -> "i"
+                "й" -> "i"
+                "к" -> "k"
+                "л" -> "l"
+                "м" -> "m"
+                "н" -> "n"
+                "о" -> "o"
+                "п" -> "p"
+                "р" -> "r"
+                "с" -> "s"
+                "т" -> "t"
+                "у" -> "u"
+                "ф" -> "f"
+                "х" -> "h"
+                "ц" -> "c"
+                "ч" -> "ch"
+                "ш" -> "sh"
+                "щ" -> "sh"
+                "ъ" -> ""
+                "ы" -> "i"
+                "ь" -> ""
+                "э" -> "e"
+                "ю" -> "yu"
+                "я" -> "ya"
+                "А" -> "A"
+                "Б" -> "B"
+                "В" -> "V"
+                "Г" -> "G"
+                "Д" -> "D"
+                "Е" -> "E"
+                "Ё" -> "E"
+                "Ж" -> "Zh"
+                "З" -> "Z"
+                "И" -> "I"
+                "Й" -> "I"
+                "К" -> "K"
+                "Л" -> "L"
+                "М" -> "M"
+                "Н" -> "N"
+                "О" -> "O"
+                "П" -> "P"
+                "Р" -> "R"
+                "С" -> "S"
+                "Т" -> "T"
+                "У" -> "U"
+                "Ф" -> "F"
+                "Х" -> "H"
+                "Ц" -> "C"
+                "Ч" -> "Ch"
+                "Ш" -> "Sh"
+                "Щ" -> "Sh"
+                "Ъ" -> ""
+                "Ы" -> "I"
+                "Ь" -> ""
+                "Э" -> "E"
+                "Ю" -> "Yu"
+                "Я" -> "Ya"
+
+                else -> it.value
+            }
+        }
+
+}
